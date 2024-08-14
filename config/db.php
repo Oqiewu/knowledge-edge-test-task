c40497fa98b7e0820e4bc4ae6b64bf30a47991bb
i.chernov2 <i.chernov04@mail.ru>
2024-08-14 16:06:57 +0400

    edit docker compose

--- config/db.php
+++ config/db.php
@@ -2,8 +2,13 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=db;dbname=yii-template-db',
-    'username' => 'yii2user',
-    'password' => 'yii2password',
+    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'username' => 'root',
+    'password' => '',
     'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
 ];
